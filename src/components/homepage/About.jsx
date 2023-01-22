0009068a1fc8e1f90be5d1376f9aa33b30c6e785
LenaWang03 <xianna03@gmail.com>
2023-01-21 23:28:21 -0800

    styled landing and half of about

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -9,9 +9,9 @@ function About() {
     <div className="about">
       <div className="about-container">
         <div className="feature-row">
-          <Features />
-          <Features />
-          <Features />
+          <Features feature={"Chatbot"} text={"Connect with a chatbot and answer questions to be redirected to the right mental health service for you."} />
+          <Features feature={"Meditation Mode"} text={"Take some time to meditate and unwind to the sound of soothing music."}/>
+          <Features feature={"Service Map"} text={"Find mental health clinics and services near you with our Mental health service Map"}/>
         </div>
         <Miu />
         <Carousel>

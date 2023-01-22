8cf7eeeee60baad3250efe3468a5fcba80cbab3e
Justin Ang <jrang@sfu.ca>
2023-01-21 16:54:25 -0800

    added leaf logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import Logo from './homepage/Logo';
 import Headline from './homepage/Headline';
+import Button from './homepage/Button';
 
 function Home() {
   return (
     <div>
       <Logo />
       <Headline />
+      <Button />
     </div>
   );
 }

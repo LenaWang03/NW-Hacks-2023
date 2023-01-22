d1aeb281710689532e36971fb02519806388ab59
Brian Rahadi <brian.rahadi@gmail.com>
2023-01-22 08:33:12 -0800

    added location leftNav and meditation music

--- src/components/homepage/LeftNav.jsx
+++ src/components/homepage/LeftNav.jsx
@@ -4,11 +4,10 @@ import { Link } from 'react-scroll';
 // import {FontAwesomeIcon}
 import HomeLogo from '../../assets/house-solid.svg'
 import MiuLogo from '../../assets/miu-icon.svg'
+import LocationLogo from '../../assets/map-location-dot-solid.svg'
 import ResourceLogo from '../../assets/resource-icon.svg'
 
 function LeftNav() {
-  const [count, setCount] = useState(0);
-
   return (
     <nav className="sidenav">
       <Link activeClass="active" className="link" smooth spy to="home" style={{cursor: 'pointer'}}>
@@ -19,6 +18,9 @@ function LeftNav() {
       </Link>
       <Link activeClass="active" className="link" smooth spy to="resources" style={{cursor: 'pointer'}}>
       <img src={ResourceLogo} className="icon"/>
+      </Link>
+      <Link activeClass="active" className="link" smooth spy to="map" style={{cursor: 'pointer'}}>
+      <img src={LocationLogo} className="icon"/>
       </Link>
     </nav>
   );
--- src/components/meditation/Timer.jsx
+++ src/components/meditation/Timer.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useTimer } from 'react-timer-hook';
+import music from '../../assets/music/music.mp3'
+
+import '../../styles/Meditation.css'
 
 function Timer({ expiryTimestamp, isMeditating, setMeditating }) {
-  // const [running, setRunning] = useState(false)
-
+  // const [audio, setAudio] = useState(null)
+  let audio = new Audio(music);
   const {
     seconds,
     minutes,
@@ -21,7 +24,7 @@ function Timer({ expiryTimestamp, isMeditating, setMeditating }) {
   return (
     
     <div style={{textAlign: 'center'}}>
-      <h1>Meditate with Miu </h1>
+      <h1 style={{color: "linear-gradient(125.35deg, #c2efba 24.24%, #bed0ff 59.9%)"}}>Meditate with Miu </h1>
       {/* <p>Timer Demo</p> */}
       <div style={{fontSize: '100px'}}>
         <span>{minutes}</span>:<span>{seconds}</span>
@@ -31,12 +34,19 @@ function Timer({ expiryTimestamp, isMeditating, setMeditating }) {
       <button onClick={pause}>Pause</button> */}
       <br/>
       <br/>
-      <button className={`${isMeditating ? "hidden" : ""}`}onClick={() => {
+      <button className={`${isMeditating ? "hidden" : ""} btn`} onClick={() => {
+        audio.loop = true;
+        audio.play()
         resume();
         setMeditating(true);
       }}>Start</button>
-      <button className={`${!isMeditating ? "hidden" : ""}`} onClick={() => {
+      <button className={`${!isMeditating ? "hidden" : ""} btn`} onClick={() => {
         // Restarts to 5 minutes timer
+        if (audio != null) {
+          audio.pause();
+          audio.currentTime = 0;
+          console.log("HIII");
+        }
         const time = new Date();
         time.setSeconds(time.getSeconds() + 600);
         restart(time);

0dabf7138a1b48a8c2296a3add3096b79c2b254c
Justin Ang <jrang@sfu.ca>
2023-01-22 00:51:19 -0800

    exported svgs

--- src/components/homepage/Landing.jsx
+++ src/components/homepage/Landing.jsx
@@ -1,23 +1,30 @@
 import Logo from './Logo';
 import Headline from './Headline';
 import Button from './Button';
-import Landing from "../../assets/Landing@4x.png";
-import divider from "../../assets/Div.png";
+import Landing from '../../assets/Landing@4x.png';
+import divider from '../../assets/Div.png';
+import Cloud1 from '../../assets/cloud1.svg';
+import Cloud2 from '../../assets/cloud2.svg';
+import Cloud3 from '../../assets/cloud3.svg';
 
 function Home() {
   return (
-    <div className = "home">
-      <div className = "landing">
-        <div className = "landing-info">
+    <div className="home">
+      <div className="clouds">
+        <img src={Cloud1} alt="Cloud 1" />
+        <img src={Cloud2} alt="Cloud 2" />
+        <img src={Cloud3} alt="Cloud 3" />
+      </div>
+      <div className="landing">
+        <div className="landing-info">
           <Logo />
           <Headline />
-          
         </div>
-        <div className = "landing-img">
-        <img className = "img-l" src={Landing} alt=""/>
+        <div className="landing-img">
+          <img className="img-l" src={Landing} alt="" />
         </div>
       </div>
-      <img className = "divider" src={divider} alt=""/>
+      <img className="divider" src={divider} alt="" />
     </div>
   );
 }

b28f2c7bcb47e6bcc6b588cf0a3d0a8930cf2d3d
Justin Ang <jrang@sfu.ca>
2023-01-21 19:56:50 -0800

    fix sizing

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -1,16 +1,18 @@
-import '../../styles/About.css'
+import '../../styles/About.css';
 import Features from './about/Features';
 import Miu from './about/Miu';
 
 function About() {
   return (
     <div className="about">
-      <div className="feature-row">
-        <Features />
-        <Features />
-        <Features />
+      <div className='about-container'>
+        <div className="feature-row">
+          <Features />
+          <Features />
+          <Features />
+        </div>
+        <Miu />
       </div>
-      <Miu />
     </div>
   );
 }

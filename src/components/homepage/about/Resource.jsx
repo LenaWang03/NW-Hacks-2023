28db6854d2119ad339421ce8eb5188d6ae17655f
Justin Ang <jrang@sfu.ca>
2023-01-22 03:03:52 -0800

    added link redirection

--- src/components/homepage/about/Resource.jsx
+++ src/components/homepage/about/Resource.jsx
@@ -1,10 +1,12 @@
-import Link from "../../../assets/Link.svg"
-import '../../../styles/Resources.css';    
+import Link from '../../../assets/Link.svg';
+import '../../../styles/Resources.css';
 
 function Resource() {
   return (
     <div className="resource">
-      <img src={Link} className="resource-link" />
+      <a type="button" href="https://www.google.com/" target="_blank" className="resource-link">
+        <img src={Link} />
+      </a>
       <h3 className="resource-header">Resource One</h3>
       <p className="resource-description">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis

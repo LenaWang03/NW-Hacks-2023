7f9479455092fd64a775055334629a8691eb62ab
Justin Ang <jrang@sfu.ca>
2023-01-21 20:27:58 -0800

    fixed css issue

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -1,17 +1,30 @@
 import '../../styles/About.css';
 import Features from './about/Features';
 import Miu from './about/Miu';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function About() {
   return (
     <div className="about">
-      <div className='about-container'>
+      <div className="about-container">
         <div className="feature-row">
           <Features />
           <Features />
           <Features />
         </div>
         <Miu />
+        <Carousel>
+          <div>
+            <Features />
+          </div>
+          <div>
+            <Features />
+          </div>
+          <div>
+            <Features />
+          </div>
+        </Carousel>
       </div>
     </div>
   );
